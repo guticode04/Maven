178be6fa265cbd347a459688bdb6de65534ce887
Gustavo Gutierrez <gusguti04@gmail.com>
2020-03-03 07:29:36 -0800

    Created sessions actions

---
+++ frontend/actions/session.js
@@ -0,0 +1,26 @@
+import {
+  postUser,
+  postSession,
+  deleteSession
+} from "../utils/session";
+
+export const RECEIVE_CURRENT_USER = "RECEIVE_CURRENT_USER";
+export const LOGOUT_CURRENT_USER = "LOGOUT_CURRENT_USER";
+
+const receiveCurrentUser = user => ({
+  type: RECEIVE_CURRENT_USER,
+  user,
+});
+
+const logoutCurrentUser = () => ({
+  type: LOGOUT_CURRENT_USER,
+});
+
+export const createNewUser = formUser => dispatch => postUser(formUser)
+  .then(user => dispatch(receiveCurrentUser(user)));
+
+export const login = formUser => dispatchEvent => postSession(formUser)
+  .then(user => dispatch(recieveCurrentUser(user)));
+
+export const logout => () => dispatch => deleteSession()
+  .then(() => dispatch(logoutCurrentUser()));
