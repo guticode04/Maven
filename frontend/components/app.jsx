be9bb06d90b3ba642ab9440bf5589e1c2f44ab87
Gustavo Gutierrez <gusguti04@gmail.com>
2020-03-04 21:02:08 -0800

    Debugging user error messages

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import SignupContainer from './session/signup_container';
 import NavBarContainer from './nav_bar/nav_bar_container';
 import { AuthRoute, ProtectedRoute } from "../utils/route_utils";
 import LoginContainer from './session/login_container';
 
 export default () => (
-  <div>
-    <Route path="/" component={NavBarContainer} />
+  <Switch>
+    <Route exact path="/" component={NavBarContainer} />
     <AuthRoute path="/signup" component={SignupContainer} />
     <AuthRoute path="/login" component={LoginContainer} />
-  </div>
+    {/* <Route path="/" component={ErrorPage} /> */}
+  </Switch>
 );
