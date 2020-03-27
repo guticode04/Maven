94aff5e3670f7594c1974af4d95d7ecdb9413370
Gustavo Gutierrez <gusguti04@gmail.com>
2020-03-27 11:09:12 -0700

    Creat annotations table

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_07_211310) do
+ActiveRecord::Schema.define(version: 2020_03_27_175957) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "annotations", force: :cascade do |t|
+    t.text "body", null: false
+    t.integer "user_id", null: false
+    t.integer "track_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["track_id"], name: "index_annotations_on_track_id"
+    t.index ["user_id"], name: "index_annotations_on_user_id"
+  end
 
   create_table "artists", force: :cascade do |t|
     t.string "name", null: false
