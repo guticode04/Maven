32f51bb944f532efbb57532b95aadb0fd7366793
Gustavo Gutierrez <gusguti04@gmail.com>
2020-05-16 18:26:27 -0700

    Add elements to footer

--- frontend/components/footer/footer.jsx
+++ frontend/components/footer/footer.jsx
@@ -10,6 +10,15 @@ const Footer = () => {
         <i className="fab fa-github" />
         <i className="fas fa-user-tie" />
       </div>
+      <div className="creator-name"> 
+        <p>Created By Gustavo Gutierrez</p>
+      </div>
+      <div className="lower-footer">
+        <p>All Artists</p>
+        <ul>
+          <li>links for alphabet of artist</li>
+        </ul>
+      </div>
     </footer>
   )
 }
