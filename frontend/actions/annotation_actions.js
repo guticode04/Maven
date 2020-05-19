11126ea1f23b430861b90c5bb955dd70f05914d6
Gustavo Gutierrez <gusguti04@gmail.com>
2020-05-18 22:12:02 -0700

    Create annotation action constants

---
+++ frontend/actions/annotation_actions.js
@@ -0,0 +1,5 @@
+import * as AnnotationApiUtil from '../utils/annotation_api_utils';
+
+export const RECEIVE_ANNOTATION = "RECEIVE_ANNOTATION";
+export const REMOVE_ANNOTATION = "REMOVE_ANNOTATION";
+
