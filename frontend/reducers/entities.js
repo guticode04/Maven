3b795a57ca38cc3bf76669d0fc5c173b89562396
Gustavo Gutierrez <gusguti04@gmail.com>
2020-05-29 21:17:51 -0700

    Add annotations reducer to entities reducer

--- frontend/reducers/entities.js
+++ frontend/reducers/entities.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import tracksReducer from './tracks';
 import usersReducer from './users_reducer';
+import annotationsReducer from './annotations_reducer';
 
 const entitiesReducer = combineReducers({
   users: usersReducer,
-  tracks: tracksReducer
+  tracks: tracksReducer,
+  annotations: annotationsReducer,
 })
 
 export default entitiesReducer;
