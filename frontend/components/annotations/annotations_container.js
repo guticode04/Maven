c5ba63c44114d2dd4e08bffd6da4296b10faa130
Gustavo Gutierrez <gusguti04@gmail.com>
2020-06-01 20:51:42 -0700

    Complete annotation container

--- frontend/components/annotations/annotations_container.js
+++ frontend/components/annotations/annotations_container.js
@@ -1,2 +1,16 @@
 import { connect } from 'react-redux';
 import { fetchAnnotation, updateAnnotation, deleteAnnotation } from '../../actions/annotation_actions';
+
+const mapStateToProps = (state, ownProps) => {
+  return ({
+    annotation: state.entities.annotation
+  })
+}
+
+const mapDispatchToProps = dispatch => ({
+  fetchAnnotation: () => dispatch(fetchAnnotation()),
+  updateAnnotation: () => dispatch(updateAnnotation()),
+  deleteAnnotation: () => dispatch(deleteAnnotation()),
+})
+
+export default connect(mapStateToProps,mapStateToProps)
--- frontend/reducers/selectors.js
+++ frontend/reducers/selectors.js
@@ -1,1 +1,7 @@
 export const selectAllTracks = state => Object.values(state.entities.tracks);
+
+// A “selector” is simply a function that accepts Redux state as an argument 
+// and returns data that is derived from that state.Bam! The end.
+// 
+//
+// Memoizing selectors
