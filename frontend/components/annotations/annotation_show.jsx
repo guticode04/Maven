663dbeebaf5d79c5f53f564ab388c12f2190648b
Gustavo Gutierrez <gusguti04@gmail.com>
2020-09-03 21:00:12 -0700

    Add classes for css on annotations show page

--- frontend/components/annotations/annotation_show.jsx
+++ frontend/components/annotations/annotation_show.jsx
@@ -7,6 +7,7 @@ class AnnotationShow extends React.Component {
 
 
   render(){
+    const {annotation} = this.props;
     return(
       <>
       <div className="annotation-container">
@@ -15,6 +16,10 @@ class AnnotationShow extends React.Component {
           <div className="triangle-top"></div>
           <div className="triangle-tip"></div>
           <div className="triangle-bottom"></div>
+        </div>
+        <div className="annotation-info">
+          <h1 className="annotation-header">Maven Annotation</h1>
+          <p className="annotation-body">{annotation.body}</p>
         </div>
       </div>
       </>
