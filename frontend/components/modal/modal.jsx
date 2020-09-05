83483f897612e9fc84938bef57337881e089b5cd
Gustavo Gutierrez <gusguti04@gmail.com>
2020-09-04 20:31:06 -0700

    Add modal folder and files

---
+++ frontend/components/modal/modal.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Modal = () => {
+
+   return(
+      <>
+         <h1> Welcome to Maven!</h1>
+         <p>
+            Maven is a music knowledge sharing app inspired by Genius.
+            Users can view tracks and their lyrics along with any comments
+            and annotation for those tracks. In order to be able to add tracks
+            comments and annotations users must create an account. 
+         </p>
+      </>
+   )
+
+}
+
+export default Modal;
