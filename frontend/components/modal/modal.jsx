01858b59475858eb5fa817e39fecaeb35078b53a
Gustavo Gutierrez <gusguti04@gmail.com>
2020-09-07 14:48:11 -0700

    Finish modal css

--- frontend/components/modal/modal.jsx
+++ frontend/components/modal/modal.jsx
@@ -9,8 +9,8 @@ const Modal = (props) => {
             <p className="modal-description">
                Maven is a music knowledge sharing app inspired by Genius.
                Users can view tracks and their lyrics along with any comments
-               and annotation for those tracks. In order to be able to add tracks
-               comments and annotations users must create an account. 
+               and annotation for those tracks. In order to be able to add tracks,
+               comments, and annotations users must be logged in or create an account. 
             </p>
             <button className="close-modal-btn" onClick={close}>Close</button>
          </div>
