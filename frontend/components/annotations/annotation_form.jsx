60e81e99caa1e79b1a0e881e4e94977cbcc74b0a
Gustavo Gutierrez <gusguti04@gmail.com>
2020-09-09 21:30:30 -0700

    Add state to annotation form and add input areas

--- frontend/components/annotations/annotation_form.jsx
+++ frontend/components/annotations/annotation_form.jsx
@@ -5,17 +5,24 @@ class AnnotationForm extends React.component {
    constructor(props) {
       super(props);
       this.state = {
-
+         body: '',
+         userId: this.props.user.id,
+         trackId: this.props.track.id,
+         start: this.props.start_idx,
+         end: this.props.end_idx,
       }
 
    }
 
-   handleSubmit() {
-
+   handleSubmit(e) {
+      e.preventDefault();
    }
 
 
    handleInput(field) {
+      return (e) => {
+         this.setState({ [ field ]: e.currentTarget.value })
+      }
 
    }
 
@@ -23,6 +30,17 @@ class AnnotationForm extends React.component {
 
       return(
          <>
+            <form className="annotation-form">
+               <label>
+                  <input>
+                  </input>
+               </label>
+               <label>
+                  <textarea>
+                     
+                  </textarea>
+               </label>
+            </form>
          </>
       )
    }
