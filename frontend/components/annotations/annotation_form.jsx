9a3e7382c5ee9534f0984fce90077a7e2bbd2e58
Gustavo Gutierrez <gusguti04@gmail.com>
2020-09-09 21:18:41 -0700

    Create annotation form jsx file and build skeleton

---
+++ frontend/components/annotations/annotation_form.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+class AnnotationForm extends React.component {
+
+   constructor(props) {
+      super(props);
+      this.state = {
+
+      }
+
+   }
+
+   handleSubmit() {
+
+   }
+
+
+   handleInput(field) {
+
+   }
+
+   render() {
+
+      return(
+         <>
+         </>
+      )
+   }
+
+}
