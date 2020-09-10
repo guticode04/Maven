2d7060fb3f0c7922f25ee6e5e4c3376429356f1b
Gustavo Gutierrez <gusguti04@gmail.com>
2020-09-10 13:50:01 -0700

    Edit annotation form

--- frontend/components/annotations/annotation_form.jsx
+++ frontend/components/annotations/annotation_form.jsx
@@ -31,18 +31,23 @@ class AnnotationForm extends React.component {
       return(
          <>
             <form className="annotation-form">
-               <label>
-                  <input>
-                  </input>
-               </label>
-               <label>
-                  <textarea>
-                     
-                  </textarea>
-               </label>
+               <textarea
+                  className="add-annotation"
+                  placeholder="Don't just put the lyrics in your own words - drop some knowledge"
+                  onChange={this.handleInput('body')}
+                  value={this.state.body}
+               />
+               <button
+                  className="add-annotation-btn"
+                  onClick={this.handleSubmit}
+               >
+                  Save
+               </button>
             </form>
          </>
       )
    }
 
 }
+
+export default AnnotationForm;
