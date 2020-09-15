b2d60533f6043048ab35739f1e0d2db88fe86fed
Gustavo Gutierrez <gusguti04@gmail.com>
2020-09-15 15:45:39 -0700

    Add new action type to reducer

--- frontend/reducers/annotations_reducer.js
+++ frontend/reducers/annotations_reducer.js
@@ -1,11 +1,14 @@
 import {
   RECEIVE_ANNOTATION,
-  REMOVE_ANNOTATION
+  REMOVE_ANNOTATION,
+  RECEIVE_ALL_ANNOTATIONS
 } from "../actions/annotation_actions";
 
 const annotationsReducer = ( oldState = {} , action ) => {
   Object.freeze(oldState);
   switch ( action.type ) {
+    case RECEIVE_ALL_ANNOTATIONS:
+      return Object.assign({}, oldState, action.annotations)
     case RECEIVE_ANNOTATION:
       return Object.assign( {}, oldState, { [action.annotation.id]: action.annotation });
     case REMOVE_ANNOTATION:
