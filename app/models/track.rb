c4b4d4d277716c98ba74ce78c55c3f33dc7792b8
Gustavo Gutierrez <gusguti04@gmail.com>
2020-09-16 22:52:10 -0700

    Fix associations bug

--- app/models/track.rb
+++ app/models/track.rb
@@ -1,16 +1,17 @@
 class Track < ApplicationRecord
 
   validates :title, :lyrics, presence: true
+  
+  has_many :annotations,
+  class_name: :Annotation,
+  foreign_key: :track_id,
+  primary_key: :id
 
   belongs_to :artist,
   class_name: :Artist,
   foreign_key: :artist_id,
   primary_key: :id
 
-  has_many :annotations,
-  class_name: :Annotation,
-  foreign_key: :track_id,
-  primary_key: :id
 
 
 end
