2064cdacb3d1eb598b4e1c137e1266e739d5c3a6
Gustavo Gutierrez <gusguti04@gmail.com>
2020-09-16 23:14:30 -0700

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,10 +5,18 @@ Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
     resources :users, only: [:index, :create, :show]
     resource :session, only: [:create, :destroy]
-    resources :tracks, except: [:new, :edit]
-    # resources :tracks, except: [:new, :edit] do
-    #   resources :annotations, only: [:index]
-    # end
+
+    # resources :tracks, except: [:new, :edit]
+
+    # here annotations belong to a tracks so we want them nested
+    # resulting route will be:
+    # get 'tracks/1/annotations'
+    # getting all the annotations for the track
+
+    resources :tracks, except: [:new, :edit] do
+      resources :annotations, only: [:index]
+    end
+    
     resources :annotations, except: [:index]
   end
 end
