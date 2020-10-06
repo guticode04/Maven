b97e280b17665689cc40543c44edfb0d869646d0
Gustavo Gutierrez <gusguti04@gmail.com>
2020-10-05 17:43:51 -0700

    Create comments table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_09_224811) do
+ActiveRecord::Schema.define(version: 2020_10_06_003549) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,16 @@ ActiveRecord::Schema.define(version: 2020_09_09_224811) do
     t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.text "body", null: false
+    t.integer "track_id", null: false
+    t.integer "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["track_id"], name: "index_comments_on_track_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "tracks", force: :cascade do |t|
