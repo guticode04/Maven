c7cfd9dea272ef9e7c08e61a81a5176cbc973987
Gustavo Gutierrez <gusguti04@gmail.com>
2020-10-05 17:48:21 -0700

    Add validations and associations to comment model

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,2 +1,14 @@
 class Comment < ApplicationRecord
+   validates :body, :track_id, :user_id, presence: true
+
+   belongs_to :track,
+   class_name: :Track,
+   foreign_key: :track_id,
+   primary_key: :id
+
+   belongs_to :user,
+   class_name: :User,
+   foreign_key: :user_id,
+   primary_key: :id
+   
 end
