da3e2004b0f566883c9f6645501f70b668d3c28b
Gustavo Gutierrez <gusguti04@gmail.com>
2020-10-07 20:36:22 -0700

    Create comment form jsx

---
+++ frontend/components/comments/comment_form.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+
+class CommentForm extends React.Component {
+   constructor(props) {
+      super(props);
+      this.state = {
+         body: '',
+         user_id: this.props.userId,
+         track_id: this.props.trackId,
+      }
+      this.handleSubmit = this.handleSubmit.bind(this);
+   }
+
+   handleSubmit(e) {
+      e.preventDefault();
+      this.props.createComment(this.state)
+   }
+
+   handleInput(field) {
+      return (e) => {
+         this.setState({ [field]: e.currentTarget.value })
+      }
+   }
+
+   render() {
+      return(
+         <>
+            <form>
+               <textarea 
+                  onChange={this.handleInput('body')}
+                  value={this.state.body}
+               />
+               <button>
+                  Save
+               </button>
+            </form>
+         </>
+      )
+   }
+
+
+}
+
+
+export default CommentForm;
