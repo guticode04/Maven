0d8282750b8aadffe2bd796668cde88d73e17ac6
Gustavo Gutierrez <gusguti04@gmail.com>
2020-10-23 13:21:13 -0700

    Add styling and ref to annotation show

--- frontend/components/annotations/annotation_show.jsx
+++ frontend/components/annotations/annotation_show.jsx
@@ -3,31 +3,75 @@ import React from 'react';
 class AnnotationShow extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      show: true,
+    }
+    this.ref = React.createRef();
+    this.hideAnnotationShow = this.hideAnnotationShow.bind(this);
   }
 
-  // Probably don't need this either
-  // componentDidMount() {
-  // //   debugger
-  //   this.props.fetchAnnotation(this.props.annotationId);
+  componentDidMount() {
+    document.addEventListener('click', this.hideAnnotationShow)
+  }
+
+  componentWillUnmount(){
+    document.removeEventListener('click', this.hideAnnotationShow);
+  }
+
+  // This works only for the first time then we lose track of
+  // current. Error says "cannot read property contains of null"
+
+  hideAnnotationShow(e) {
+      e.preventDefault();
+      this.setState( { show: false });
+      if ( !this.ref.current.contains(e.target) ) {
+        this.setState( { show: true });
+        document.removeEventListener('click', this.hideAnnotationShow);
+      }
+      document.addEventListener('click', this.hideAnnotationShow)
+  }
+
+  // THIS DOES NOT WORK!!
+
+  // hideAnnotationShow(e) {
+  //   if ( this.state.show === false ) {
+  //     e.preventDefault();
+  //     this.setState( { show: true });
+  //     this.showAnnotationShow = (e) => {
+  //       if ( !this.ref.current.contains(e.target) ) {
+  //         this.setState( { show: false } );
+  //         document.removeEventListener('click', this.showAnnotationShow);
+  //         this.showAnnotationShow = null;
+  //       }
+  //     }
+  //     document.addEventListener('click', this.showAnnotationShow)
+  //   }
   // }
 
   render(){
     const { annotation } = this.props;
-    // debugger
     return(
       <>
-      <div className="annotation-container">
-        <div className="annotation-divider"></div>
-        <div className="annotation-triangle-container">
-          <div className="triangle-top"></div>
-          <div className="triangle-tip"></div>
-          <div className="triangle-bottom"></div>
-        </div>
-        <div className="annotation-info">
-          <h1 className="annotation-header">Maven Annotation</h1>
-          <p className="annotation-body">{annotation.body}</p>
-        </div>
-      </div>
+      {
+        this.state.show ?
+          // null
+          //   : 
+          <div className="annotation-container" ref={this.ref}>
+            <div className="annotation-divider"></div>
+            <div className="annotation-triangle-container">
+              <div className="triangle-top"></div>
+              <div className="triangle-tip"></div>
+              <div className="triangle-bottom"></div>
+            </div>
+            <div className="annotation-info">
+              <h1 className="annotation-header">Maven Annotation</h1>
+              <p className="annotation-body">{annotation.body}</p>
+            </div>
+          </div>
+          
+          : 
+          null
+      }
       </>
     )
   }
