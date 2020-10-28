67a77c76fd3e30e029439840b261b5e11018b561
Gustavo Gutierrez <gusguti04@gmail.com>
2020-10-27 22:39:37 -0700

    Minor update in annotations show and continue debugging

--- frontend/components/annotations/annotation_show.jsx
+++ frontend/components/annotations/annotation_show.jsx
@@ -5,7 +5,6 @@ class AnnotationShow extends React.Component {
     super(props);
     this.state = {
       show: true,
-      // show: false,
     }
     this.ref = React.createRef();
     this.hideAnnotationShow = this.hideAnnotationShow.bind(this);
@@ -13,29 +12,12 @@ class AnnotationShow extends React.Component {
 
   componentDidMount() {
     document.addEventListener('click', this.hideAnnotationShow)
-    // this.hideAnnotationShow();
   }
 
   componentWillUnmount(){
-    // if ( this.showAnnotationShow ) {
-      document.removeEventListener('click', this.showAnnotationShow);
-    // }
+    document.removeEventListener('click', this.hideAnnotationShow);
+    this.setState({ show: true })
   }
-
-  // hideAnnotationShow(e) {
-  //   if (this.state.show === false) {
-  //     e.preventDefault();
-  //     this.setState({ show: true });
-  //     this.showAnnotationShow = (e) => {
-  //       if ( !this.ref.current.contains(e.target)) {
-  //         this.setState({ show: false });
-  //         document.removeEventListener('click', this.showAnnotationShow);
-  //         this.showAnnotationShow = null;
-  //       }
-  //     }
-  //     document.addEventListener('click', this.showAnnotationShow)
-  //   }
-  // }
 
   // This works only for the first time then we lose track of
   // current. Error says "cannot read property contains of null"
@@ -46,9 +28,7 @@ class AnnotationShow extends React.Component {
       this.setState( { show: false });
       if ( this.ref.current && !this.ref.current.contains(e.target) ) {
         this.setState( { show: true });
-        document.removeEventListener('click', this.hideAnnotationShow);
       }
-      document.addEventListener('click', this.hideAnnotationShow)
   }
 
 
